9eb2840bc898ba1c2be0722c072e385fd8bc3fdf
Jon Frank <github@molokoplus.org>
2020-11-08 07:11:04 -0600

    LED Mux work,  organized vars

--- forms.h
+++ forms.h
@@ -1,6 +1,130 @@
 #ifndef _FORMS_H
 #define _FORMS_H
 
-enum FORMS {MAJ, MIN, DIM, SUS2, SUS4};
+enum FORMS {MAJ, MIN, DIM, SUS2, SUS4, MAJ3, MIN3, FIFTH, ROOTONLY, UNDEF};
+
+
+static int SCALE_UNDEF[12] =       {-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1};
+
+static int SCALE_CHRO[12] =    { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9,10,11};
+static int SCALE_MAJ_DIA[12] = { 0, 2, 4, 5, 7, 9,11,-1,-1,-1,-1,-1};
+static int SCALE_MIN_DIA[12] = { 0, 2, 3, 5, 7, 9,10,-1,-1,-1,-1,-1};
+
+static int TRIAD_MAJ[12] =     { 0, 4, 7,-1,-1,-1,-1,-1,-1,-1,-1,-1}; //0
+static int TRIAD_MIN[12] =     { 0, 3, 7,-1,-1,-1,-1,-1,-1,-1,-1,-1}; //1
+static int TRIAD_DIM[12] =     { 0, 3, 6,-1,-1,-1,-1,-1,-1,-1,-1,-1}; //2
+static int TRIAD_SUS2[12] =    { 0, 2, 7,-1,-1,-1,-1,-1,-1,-1,-1,-1}; //3
+static int TRIAD_SUS4[12] =    { 0, 5, 7,-1,-1,-1,-1,-1,-1,-1,-1,-1}; //4
+
+static int IVL_MAJ_THIRD[12] = { 0, 4,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1};
+static int IVL_MIN_THIRD[12] = { 0, 3,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1};
+static int IVL_FIFTH[12] =     { 0, 7,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1};
+
+static int ROOT_ONLY[12] = { 0,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1};
+
+
+
+static int CHROMATIC_PROG[12][2] = {      {0,MAJ},
+                                          {1,ROOTONLY},
+                                          {2,ROOTONLY},
+                                          {3,ROOTONLY},
+                                          {4,ROOTONLY},
+                                          {5,ROOTONLY},
+                                          {6,MAJ}, 
+                                          {7,ROOTONLY},
+                                          {8,ROOTONLY},
+                                          {9,MAJ},
+                                          {10,ROOTONLY},
+                                          {11,ROOTONLY}};
+
+static int MAJ_CHORD_PROG[12][2] = {      {0,MAJ},
+                                          {2,MIN},
+                                          {4,MIN},
+                                          {5,MAJ},
+                                          {7,MAJ},
+                                          {9,MIN},
+                                          {11,DIM},  //WTF... should be 11?  But 12 plays correctly???
+                                          {-1,UNDEF},
+                                          {-1,UNDEF},
+                                          {-1,UNDEF},
+                                          {-1,UNDEF},
+                                          {-1,UNDEF}};
+
+                                          
+static int MIN_CHORD_PROG[12][2] = {      {0,MIN},
+                                          {2,DIM},
+                                          {3,MAJ},
+                                          {5,MIN},
+                                          {7,MIN},
+                                          {8,MAJ},
+                                          {10,MAJ},  // SAME... should be 10...?? but 11 is better?
+                                          {-1,UNDEF},
+                                          {-1,UNDEF},
+                                          {-1,UNDEF},
+                                          {-1,UNDEF},
+                                          {-1,UNDEF}};
+
+
+
+/*
+static byte SCALE_CHRO[12] = {0,1,2,3,4,5,6,7,8,9,10,11};
+static byte SCALE_MAJ_DIA[7] = {0,2,4,5,7,9,11};
+static byte SCALE_MIN_DIA[7] = {0,2,3,5,7,9,10};
+
+static byte TRIAD_MAJ[3] = {0,4,7}; //0
+static byte TRIAD_MIN[3] = {0,3,7}; //1
+static byte TRIAD_DIM[3] = {0,3,6}; //2
+static byte TRIAD_SUS2[3] = {0,2,7}; //3
+static byte TRIAD_SUS4[3] = {0,5,7}; //4
+
+static byte INTERVAL_MAJ_THIRD[3] = {0,4};
+static byte INTERVAL_MIN_THIRD[3] = {0,3};
+static byte INTERVAL_FIFTH[3] = {0,7};
+
+static byte MAJ_CHORD_PROG[7][2] = { {0,MAJ},
+                                          {2,MIN},
+                                          {4,MIN},
+                                          {5,MAJ},
+                                          {7,MAJ},
+                                          {9,MIN},
+                                          {11,DIM} };
+
+                                          
+static byte MIN_CHORD_PROG[7][2] = { {0,MIN},
+                                          {2,DIM},
+                                          {3,MAJ},
+                                          {5,MIN},
+                                          {7,MIN},
+                                          {8,MAJ},
+                                          {10,MAJ} };
+
+*/
+
+
+int * chordFromForm(int form){
+    switch(form){
+      case MAJ:
+        return TRIAD_MAJ;
+        break;
+      case MIN:
+        return TRIAD_MIN;
+        break;
+      case DIM:
+        return TRIAD_DIM;
+        break;
+      case SUS2:
+        return TRIAD_SUS2;
+        break;
+      case SUS4:
+        return TRIAD_SUS4;
+        break;
+      case ROOTONLY:
+        return ROOT_ONLY;
+        break;
+      default:
+        return SCALE_UNDEF;
+    }
+ //maj min dim sus2 sus4  
+}
 
 #endif // _FORMS_H
--- miditropolis.ino
+++ miditropolis.ino
@@ -7,69 +7,86 @@
 
 MIDI_CREATE_DEFAULT_INSTANCE();
 
-static const unsigned ledPin = 53;      // LED pin on Arduino Uno
+//mux pinout
+const bool muxTruthTable[16][4] = { {0,0,0,0},
+                                    {1,0,0,0},
+                                    {0,1,0,0},
+                                    {1,1,0,0},
+                                    {0,0,1,0},
+                                    {1,0,1,0},
+                                    {0,1,1,0},
+                                    {1,1,1,0},
+                                    {0,0,0,1},
+                                    {1,0,0,1},
+                                    {0,1,0,1},
+                                    {1,1,0,1},
+                                    {0,0,1,1},
+                                    {1,0,1,1},
+                                    {0,1,1,1},
+                                    {1,1,1,1},
+                                   };
 
-static const int SCALE_MAJ_DIA[7] = {0,2,4,5,7,9,11};
-static const int SCALE_MIN_DIA[7] = {0,2,3,5,7,9,10};
+//pin constants
+  static const unsigned clkPin = 52;   
+  static const unsigned ledMuxPins[5] = {22,23,24,25,53};
 
-static const int TRIAD_MAJ[3] = {0,4,7}; //0
-static const int TRIAD_MIN[3] = {0,3,7}; //1
-static const int TRIAD_DIM[3] = {0,3,6}; //2
-static const int TRIAD_SUS2[3] = {0,2,7}; //3
-static const int TRIAD_SUS4[3] = {0,5,7}; //4
+//timing variables
+int clk = 0; //counter for midi clock pulses (24 per quarter note)
+bool noteOn = false; //flag for whether a note is actually playing
+int stepindex = 0; //counter for sequencer steps
+int stepLengthIndex = 0; //counter for clock divisions within a seq step (longer notes than others, etc)
 
-static const int INTERVAL_MAJ_THIRD[3] = {0,4};
-static const int INTERVAL_MIN_THIRD[3] = {0,3};
-static const int INTERVAL_FIFTH[3] = {0,7};
+//timing constants
+const int clkMax=96*4; //clock will count every 4 measures (not really a big deal, the bigger, the long divisions possible)
 
-static const int MAJ_CHORD_PROG[7][2] = { {0,MAJ},
-                                          {2,MIN},
-                                          {4,MIN},
-                                          {5,MAJ},
-                                          {7,MAJ},
-                                          {9,MIN},
-                                          {11,DIM} };
-
-                                          
-static const int MIN_CHORD_PROG[7][2] = { {0,MIN},
-                                          {2,DIM},
-                                          {3,MAJ},
-                                          {5,MIN},
-                                          {7,MIN},
-                                          {8,MAJ},
-                                          {10,MAJ} };
-
-int clk = 0;
-int clkOffset = (24*4); //processing lag... allow a clock run-up to next measure to sync at beginning
+//hacky timing variables -- modify these to tweak the processing lag between clock rcv and note out
 bool hasOffset = false;
+int clkOffset = (24*4); //processing lag... allow a clock run-up to next measure to sync at beginning
 
-int clkMax=96*4;
+
+
+
+//global settings
+int g_scale[12][2] = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
+
 int clkDivider = 96/16; //  96/n  for nth notes  96*n for superwhole
+int g_root = 60; //read singular root knob/property
+
+int stepMax = 8; //should be done away with after -1 testing?
 
 
-int stepindex = 0;
-int stepMax = 4;
+//value testing data -- before inputs avail
+int in_note[8][2] =    {{0, 1},  //octave, position
+                        {0, 2},
+                        {0, 3},
+                        {0, 4},
+                        {0, 5},
+                        {0, 6},
+                        {0, 7},
+                        {1, 1}
+                       };
+int in_velocity[8] = {110,90,80,90, 50,50,50,50}; //velocity value == <0 indicates reset steps (needs testing)
+int in_length[8] = {1,1,1,1, 1,1,1,1}; // how many clock divs to count for this step
+int in_duration[8] = {0,0,0,0, 0,0,0,0};  //How long each note lasts -- TBD, may also be a gate mode (whether for longer lengths note repeated, or once)  
+                                          //^^^??? First half 1 hit in increments of length setting, second half all hits in increments of length                              
 
-bool noteOn = false;
-int progroot = 62;
-int progression[4][2] = {   {0,1},
-                              {-1,7},
-                              {-1,5},
-                              {-1,6}
-                              };
-  
 
 
-void setup()
-{
-    pinMode(ledPin, OUTPUT);
+void setup(){
+    setGlobalScale(MAJ_CHORD_PROG);
+    
+    pinMode(ledMuxPins[0], OUTPUT);
+    pinMode(ledMuxPins[1], OUTPUT);
+    pinMode(ledMuxPins[2], OUTPUT);
+    pinMode(ledMuxPins[3], OUTPUT);
+    pinMode(ledMuxPins[4], OUTPUT);
+    
     MIDI.begin(MIDI_CHANNEL_OMNI);                      // Launch MIDI and listen to all channels
     MIDI.setHandleStart(handleStart);
     MIDI.setHandleClock(handleClock);
 }
 
-void loop()
-{
+void loop(){
   MIDI.read();    
 }
 
@@ -77,21 +94,29 @@ void handleStart(void){
   clk = 0;
   if(hasOffset)
     clk = 0-clkOffset;
-  noteOn = false;  
+  noteOn = false; 
+  stepindex = 0;
+  stepLengthIndex = 0; 
 }
+//TODO write handleStop -- when DAW sends stop... housekeeping stuff like turn off LEDs, reset clocks to 0?
+
 
 //attempt at handling via a measure clock and subdividing from it
 void handleClock(void){    
     
     if(clk>=0 && (clk%clkDivider==0)){
-      //clock is on the trigger pulse for a step 
-      stepOn(progroot, progression, stepindex);
+      //clock is on the trigger pulse for a step      
+      stepOn(g_root, g_scale, in_note[stepindex][0], in_note[stepindex][1], in_velocity[stepindex]);      
+      writeMuxLED(stepindex,HIGH);
+      //digitalWrite(clkPin,HIGH);
     }
     
     if(clk>=0 && ((clk+1)%clkDivider==0) ){
       //clock is on the pulse before next trigger, turn off last step's note
-      if(noteOn){
-        stepOff(progroot, progression, stepindex);
+      if(noteOn){        
+        stepOff(g_root, g_scale, in_note[stepindex][0], in_note[stepindex][1]);        
+        //digitalWrite(clkPin,LOW);
+        writeMuxLED(stepindex,LOW);
         stepindex++;
       }          
     }
@@ -100,129 +125,166 @@ void handleClock(void){
     if(clk>clkMax-1){
       clk = 0;
     }
+    
     if(stepindex>stepMax-1){
       stepindex = 0;
     }
 }
 
 
+////////////////////////////////////////
+//for variable-defined progressions
+//min chord prog => int scale[12][2]
+//prog => byte octaveOffset, byte scaleIndex
 
-void stepOn(int root, int prog[4][2], int i){
+//scale
+//octave + scalevalue
+
+void stepOn(int root, int scale[12][2], int octaveOffset, int scaleIndex, int velocity){
   
   if(!noteOn){
-    chordOn(  root+(12*prog[i][0]) +MIN_CHORD_PROG[prog[i][1]-1][0], 
-              chordFromForm(MIN_CHORD_PROG[prog[i][1]-1][1]));
+    chordOn(  root+ (octaveOffset*12) + scale[scaleIndex-1][0], 
+              chordFromForm(scale[scaleIndex-1][1]),
+              velocity);
     noteOn = true;
   }
 }
-
-void stepOff(int root, int prog[4][2], int i){
-    chordOff(  root+(12*prog[i][0]) +MIN_CHORD_PROG[prog[i][1]-1][0], 
-              chordFromForm(MIN_CHORD_PROG[prog[i][1]-1][1]));
+void stepOff(int root, int scale[12][2], int octaveOffset, int scaleIndex){
+  
+    chordOff(  root+ (octaveOffset*12) + scale[scaleIndex-1][0], 
+              chordFromForm(scale[scaleIndex-1][1]));
     noteOn = false;
-    
-    
+}
+//////////////////////////////////////
+
+
+
+
+void chordOn(int key, int chord[12], int velocity){
+        for (int i=0; i<12; i++){
+          if(chord[i]<0)
+            break;
+           
+          MIDI.sendNoteOn(key+chord[i], velocity, 1);
+          digitalWrite(clkPin, HIGH);
+          
+        }
+}
+
+void chordOff(int key, int chord[12]){
+  
+        for (int i=0; i<12; i++){
+          if(chord[i]<0)
+            break;
+          MIDI.sendNoteOff(key+chord[i], 0, 1);
+          digitalWrite(clkPin,LOW);
+                                   
+        }
 }
 
 
-void playProgression(int root, int duration, int rest, int prog[4][2]){ 
-  for(int i=0;i<4;i++){
-    playChord(  root+(12*prog[i][0]) +MIN_CHORD_PROG[prog[i][1]-1][0], 
-              chordFromForm(MIN_CHORD_PROG[prog[i][1]-1][1]), 
-              duration);
-    delay(rest);  
+void writeMuxLED(int muxIndex, bool on){
+  //set digital pins to mux selector
+  for(int i=0; i<4; i++){
+    digitalWrite(ledMuxPins[i], muxTruthTable[muxIndex][i]);
   }
-  
+  //write signal
+  digitalWrite(ledMuxPins[4], on);
 }
 
+/////UNCOMMENT ME WHEN READY FOR MULTIPLEXING INPUTS
+//int readMuxValue(int muxGroup, int muxIndex){
+//  //set digital pins to mux selector
+//  for(int i=0; i<4; i++){
+//    digitalWrite(inputMuxPins[i], muxTruthTable[muxIndex][i]);  //todo: add another dimension to inputMuxPins array... chooses which multiplexer to address for input
+//  }
+//  //write signal
+//  analogRead(inputMuxPins[4], on);
+//}
 
-int * chordFromForm(int form){
-    switch(form){
-      case MAJ:
-        return TRIAD_MAJ;
-        break;
-      case MIN:
-        return TRIAD_MIN;
-        break;
-      case DIM:
-        return TRIAD_DIM;
-        break;
-      case SUS2:
-        return TRIAD_SUS2;
-        break;
-      case SUS4:
-        return TRIAD_SUS4;
-        break;
-      default:
-        return TRIAD_MAJ;
-    }
- //maj min dim sus2 sus4  
+
+void setGlobalScale(int newscale[12][2]){
+  for (int i=0; i<12; i++){
+    g_scale[i][0] = newscale[i][0];
+    g_scale[i][1] = newscale[i][1];  
+  }  
 }
 
+///////////////////////////////DEPRECATED OLD STUFF BELOW ///////////////////////////////////
 
-void chordOn(int root, int chord[3]){
-        if(root > 120)
-          root = 120;
-  
-        MIDI.sendNoteOn(root+chord[0], 90, 1);
-        MIDI.sendNoteOn(root+chord[1], 90, 1);
-        MIDI.sendNoteOn(root+chord[2], 90, 1);
 
-}
-void chordOff(int root, int chord[3]){
-        if(root > 120)
-          root = 120;
-  
-        MIDI.sendNoteOff(root+chord[0], 0, 1);               
-        MIDI.sendNoteOff(root+chord[1], 0, 1);
-        MIDI.sendNoteOff(root+chord[2], 0, 1);  
-  
-}
+////////////////////////////////////////
+////for self contained progressions (hard coded arrays)
+//void stepOn(int root, int prog[4][2], int i, int velocity){
+//  
+//  if(!noteOn){
+//    chordOn(  root+(12*prog[i][0]) +MIN_CHORD_PROG[prog[i][1]-1][0], 
+//              chordFromForm(MIN_CHORD_PROG[prog[i][1]-1][1]),velocity);
+//    noteOn = true;
+//  }
+//}
+//void stepOff(int root, int prog[4][2], int i){
+//    chordOff(  root+(12*prog[i][0]) +MIN_CHORD_PROG[prog[i][1]-1][0], 
+//              chordFromForm(MIN_CHORD_PROG[prog[i][1]-1][1]));
+//    noteOn = false;
+//}
+///////////////////////////////////////////
 
-void playChord(int root, int chord[3], int duration){
-    
-        if(root > 120)
-          root = 120;
-  
-        MIDI.sendNoteOn(root+chord[0], 90, 1);
-        MIDI.sendNoteOn(root+chord[1], 90, 1);
-        MIDI.sendNoteOn(root+chord[2], 90, 1);
 
-        delay(duration);
-        
-        MIDI.sendNoteOff(root+chord[0], 0, 1);               
-        MIDI.sendNoteOff(root+chord[1], 0, 1);
-        MIDI.sendNoteOff(root+chord[2], 0, 1);  
-      
-}
+//void playProgression(int root, int duration, int rest, int prog[4][2]){ 
+//  for(int i=0;i<4;i++){
+//    playChord(  root+(12*prog[i][0]) +MIN_CHORD_PROG[prog[i][1]-1][0], 
+//              chordFromForm(MIN_CHORD_PROG[prog[i][1]-1][1]), 
+//              duration);
+//    delay(rest);  
+//  }
+//  
+//}
 
-void playSeq(int root, int chord[3]){
-  digitalWrite(ledPin, HIGH);
-        if(root > 120)
-          root = 120;
-  
-        MIDI.sendNoteOn(root+chord[0], 70, 1);
-        delay(500);               
-        MIDI.sendNoteOff(root+chord[0], 0, 1);     
-        delay(100);
-        digitalWrite(ledPin, LOW);
-
-        MIDI.sendNoteOn(root+chord[1], 90, 1);
-        delay(500);               
-        MIDI.sendNoteOff(root+chord[1], 0, 1);     
-        delay(50);
-
-        MIDI.sendNoteOn(root+chord[2], 110, 1);
-        delay(500);               
-        MIDI.sendNoteOff(root+chord[2], 0, 1);     
-        delay(100);
-
-        MIDI.sendNoteOn(root+chord[1], 90, 1);
-        delay(500);               
-        MIDI.sendNoteOff(root+chord[1], 0, 1);     
-        delay(50);
-      
-}
+//void playChord(int root, int chord[3], int duration){
+//    
+//        if(root > 120)
+//          root = 120;
+//  
+//        MIDI.sendNoteOn(root+chord[0], 90, 1);
+//        MIDI.sendNoteOn(root+chord[1], 90, 1);
+//        MIDI.sendNoteOn(root+chord[2], 90, 1);
+//
+//        delay(duration);
+//        
+//        MIDI.sendNoteOff(root+chord[0], 0, 1);               
+//        MIDI.sendNoteOff(root+chord[1], 0, 1);
+//        MIDI.sendNoteOff(root+chord[2], 0, 1);  
+//      
+//}
+//
+//void playSeq(int root, int chord[3]){
+//  digitalWrite(ledPin, HIGH);
+//        if(root > 120)
+//          root = 120;
+//  
+//        MIDI.sendNoteOn(root+chord[0], 70, 1);
+//        delay(500);               
+//        MIDI.sendNoteOff(root+chord[0], 0, 1);     
+//        delay(100);
+//        digitalWrite(ledPin, LOW);
+//
+//        MIDI.sendNoteOn(root+chord[1], 90, 1);
+//        delay(500);               
+//        MIDI.sendNoteOff(root+chord[1], 0, 1);     
+//        delay(50);
+//
+//        MIDI.sendNoteOn(root+chord[2], 110, 1);
+//        delay(500);               
+//        MIDI.sendNoteOff(root+chord[2], 0, 1);     
+//        delay(100);
+//
+//        MIDI.sendNoteOn(root+chord[1], 90, 1);
+//        delay(500);               
+//        MIDI.sendNoteOff(root+chord[1], 0, 1);     
+//        delay(50);
+//      
+//}
 
 /* 
 C  |  D  |  E  F  |  G  |  A  |  B  C
